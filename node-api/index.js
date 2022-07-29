6ebf084c9fd24906a597727dfe1429975a1936a7
mariamprieto <mariaprieto@byui.edu>
2022-07-29 15:11:29 -0400

    server running

---
+++ node-api/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql2')
+const myconn = require('express-myconnection')
+const routes = require('./routes')
+const cors = require('cors')
+
+app.set('port', process.env.PORT || 3002)
+
+const dbOptions = {
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'abc123',
+    database: 'escola',
+}
+
+//middlewares--------------------------
+app.use(myconn(mysql, dbOptions, 'single'))
+app.use(express.json())
+app.use(cors())
+//routes--------------------------------
+app.get('/', (req, res) => {
+    res.send('working')
+})
+
+app.use('/api', routes)
+//server running-------------------------
+app.listen(app.get('port'), () => {
+    console.log('server running on port', app.get('port'))
+})
---
+++ node-api/routes.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const routes = express.Router()
+
+routes.get('/', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
+        conn.query('SELECT * FROM detalhes_courses', (err, rows) => {
+            if (err) return res.send(err)
+            res.send(rows)
+
+        })
+    })
+})
+
+routes.post('/', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
+
+        conn.query('INSERT INTO detalhes_courses set ?', [req.body], (err, rows) => {
+            if (err) return res.send(err)
+
+            res.send('courses added')
+
+        })
+    })
+})
+
+
+routes.delete('/:idCourse', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
+
+        conn.query('DELETE FROM detalhes_courses WHERE idCourse= ?', [req.params.idCourse], (err, rows) => {
+            if (err) return res.send(err)
+
+            res.send('courses delete')
+
+        })
+    })
+})
+
+routes.put('/:idCourse', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err)
+
+        conn.query('UPDATE detalhes_courses set ? WHERE idCourse= ?', [req.body, req.params.idCourse], (err, rows) => {
+            if (err) return res.send(err)
+
+            res.send('courses update')
+
+        })
+    })
+})
+
+
+
+
+
+module.exports = routes
