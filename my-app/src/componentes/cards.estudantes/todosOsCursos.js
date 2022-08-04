943fc25d4d48c126433c119709556a0216efa925
mildred morales <mildredmoralesalmeida1503@gmail.com>
2022-08-04 11:24:42 -0300

    limpando

--- my-app/src/componentes/cards.estudantes/todosOsCursos.js
+++ my-app/src/componentes/cards.estudantes/todosOsCursos.js
@@ -32,7 +32,7 @@ console.log(detalhes_courses.idCourse)
   var idEstudantes=localStorage.getItem("auth")
   
   console.log(idEstudantes)
-  //const [inscricao, setInscricao] = useState(0)
+  const [inscricao, setInscricao] = useState(0)
 
   
   const OnAdd = (idCourse) => {
@@ -59,12 +59,12 @@ console.log(detalhes_courses.idCourse)
 
 
 
-    /*reiniciar el state
+    //reiniciar el state
     setInscricao({
       id_estudantes: '',
       id_courses: '',
       
-    })*/
+    })
 
   }
 
